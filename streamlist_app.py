37881b7e1c50068eb876033fe965ce0cb66d31ec
aniketlearning <145567303+aniketlearning@users.noreply.github.com>
2023-09-20 18:24:05 +0530

    Update streamlist_app.py

--- streamlist_app.py
+++ streamlist_app.py
@@ -3,5 +3,5 @@ import streamlit
 streamlit.title('My Name is Aniket Yadav')
 streamlit.header('Breakfast menu')
 streamlit.text('Panner and Chapati')
-steamlit.text('Dahi Jalebi')
+streamlit.text('Dahi Jalebi')
 streamlit.text('Chola Bhatura')
