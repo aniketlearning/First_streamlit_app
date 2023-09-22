ee97e7ced013eff2d4acf9fbe4dea53eb7435a1b
aniketlearning <145567303+aniketlearning@users.noreply.github.com>
2023-09-22 12:51:33 +0530

    Update streamlist_app.py

--- streamlist_app.py
+++ streamlist_app.py
@@ -1,4 +1,8 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Name is Aniket Yadav')
 
@@ -12,7 +16,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
 
-import pandas
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -37,7 +41,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 #streamlit.text(fruityvice_response.json()) # just write the data to the screen
@@ -50,10 +54,11 @@ streamlit.dataframe(fruityvice_normalized)
 
 # Display the table on the page.
 
-
+# don't run the code after that
+streamlit.stop()
 
 # connecting througt the snowflake connectors
-import snowflake.connector
+
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -68,7 +73,7 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Jackfruit')
 streamlit.write('The user entered ', fruit_choice)
