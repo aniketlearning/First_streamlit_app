5205fbf0858cdef43a004106847421b49f54bd4d
aniketlearning <145567303+aniketlearning@users.noreply.github.com>
2023-09-22 14:57:38 +0530

    Update streamlist_app.py

--- streamlist_app.py
+++ streamlist_app.py
@@ -94,18 +94,30 @@ streamlit.stop()
 # connecting througt the snowflake connectors
 
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-# my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_cur.execute("SELECT * from fruit_load_list")
-# my_data_row = my_cur.fetchone()
-my_data_rows = my_cur.fetchall()
-# streamlit.text("Hello from Snowflake:")
-# streamlit.text("The fruit load list contains:")
-# streamlit.text(my_data_row)
+# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+# my_cur = my_cnx.cursor()
+# # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+# my_cur.execute("SELECT * from fruit_load_list")
+# # my_data_row = my_cur.fetchone()
+# my_data_rows = my_cur.fetchall()
+# # streamlit.text("Hello from Snowflake:")
+# # streamlit.text("The fruit load list contains:")
+# # streamlit.text(my_data_row)
 
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+# snowflake related function
+def get_fruit_load_list():
+    with  my_cnx.cursor() as my_cur:
+        my_cur.execute("SELECT * from fruit_load_list")
+        return my_cur.fetchall()
+
+# Add buttion to load the fruit
+if streamlit.button('Get fruit load list'):
+    snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
+
+# streamlit.dataframe(my_data_rows)
 
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
